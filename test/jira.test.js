46094c9718c18e0a9186c6a933e990ec0641247e
pineoc <kingsuk92@gmail.com>
2018-10-02 01:16:08 +0900

    update test file, add sinon module for spy

---
+++ test/jira.test.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const sinon = require('sinon')
+const EventEmitter = require('events').EventEmitter;
+const jiraService = require('../routes/jiraService');
+
+describe('jiraService module test', function () {
+  describe('getIssueByKey() test', function () {
+    it('Should get JIRA issue data fields not null', function (done) {
+      jiraService.getIssueByKey('BRO-1234', function (res) {
+        assert.notEqual(res.fields, null);
+        done();
+      });
+    });
+    it('Should not get JIRA issue data. fields undefined', function (done) {
+      jiraService.getIssueByKey('BRO1-1234', function (res) {
+        assert.equal(res.fields, undefined);
+        done();
+      });
+    });
+  });
+  describe('getIssueByKeyFiltered() test', function () {
+    it('Should get JIRA issue key is same', function (done) {
+      jiraService.getIssueByKeyFiltered('BRO-1234', function (res) {
+        assert.equal(res.key, 'BRO-1234');
+        done();
+      });
+    });
+    it('Should not get JIRA issue. key undefined', function (done) {
+      jiraService.getIssueByKeyFiltered('BRO1-1234', function (res) {
+        assert.equal(res.key, undefined);
+        done();
+      });
+    });
+    it('Should get JIRA issue data fields fix Version', function (done) {
+      jiraService.getIssueByKeyFiltered('BRO-11234', function (res) {
+        assert.notEqual(res.fixVersion, null);
+        done();
+      });
+    });
+  });
+});
--- test/test.js
+++ test/test.js
@@ -1,70 +1,26 @@
-var assert = require('assert');
-var slackService = require('../routes/slackService');
-var jiraService = require('../routes/jiraService');
+const assert = require('assert');
+const sinon = require('sinon');
+const EventEmitter = require('events').EventEmitter;
 
-describe('slackService module test', function () {
-  describe('checkText() success test', function () {
-    const testText = 'Hello! PRO-1234, PRO-1235 PRO-9999';
-    let parsedText;
-    before(function () {
-      slackService.checkTextForJiraTicket(testText, (res) => {
-        parsedText = res;
-      });
-    });
-    it('Should get jira 3 tickets on text', function () {
-      assert.equal(parsedText.length, 3);
-    });
-    it('Should get jira ticket be in order. first key === PRO-1234', function () {
-      assert.equal(parsedText[0], 'PRO-1234');
-    });
-    it('Should get jira ticket 1 array', function () {
-      slackService.checkTextForJiraTicket('Good! PRO-1111', (res) => {
-        assert.equal(res.length, 1);
-        assert.equal(res[0], 'PRO-1111');
-      });
-    });
-    it('Should get jira ticket on URL', function () {
-      slackService.checkTextForJiraTicket('Good! https://example.com/browse/PRO-11296 PRO-1111', (res) => {
-        assert.equal(res.length, 2);
-        assert.equal(res[0], 'PRO-11296');
-      });
-    });
-  });
-});
+describe('EventEmitter', function(){
+  describe('#emit()', function(){
+    it('should invoke the callback', function(){
+      var spy = sinon.spy()
+        , emitter = new EventEmitter();
 
-describe('jiraService module test', function () {
-  describe('getIssueByKey() test', function () {
-    it('Should get JIRA issue data fields not null', function (done) {
-      jiraService.getIssueByKey('BRO-1234', function (res) {
-        assert.notEqual(res.fields, null);
-        done();
-      });
+      emitter.on('foo', spy);
+      emitter.emit('foo');
+      assert.equal(spy.called, true);
     });
-    it('Should not get JIRA issue data. fields undefined', function (done) {
-      jiraService.getIssueByKey('BRO1-1234', function (res) {
-        assert.equal(res.fields, undefined);
-        done();
-      });
-    });
-  });
-  describe('getIssueByKeyFiltered() test', function () {
-    it('Should get JIRA issue key is same', function (done) {
-      jiraService.getIssueByKeyFiltered('BRO-1234', function (res) {
-        assert.equal(res.key, 'BRO-1234');
-        done();
-      });
-    });
-    it('Should not get JIRA issue. key undefined', function (done) {
-      jiraService.getIssueByKeyFiltered('BRO1-1234', function (res) {
-        assert.equal(res.key, undefined);
-        done();
-      });
-    });
-    it('Should get JIRA issue data fields fix Version', function (done) {
-      jiraService.getIssueByKeyFiltered('BRO-11234', function (res) {
-        assert.notEqual(res.fixVersion, null);
-        done();
-      });
+
+    it('should pass arguments to the callbacks', function(){
+      var spy = sinon.spy()
+        , emitter = new EventEmitter();
+
+      emitter.on('foo', spy);
+      emitter.emit('foo', 'bar', 'baz');
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWith(spy, 'bar', 'baz');
     });
   });
 });
