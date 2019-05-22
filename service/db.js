359d1cd11389983e419cfe5c6199d046f4585b62
pineoc <kingsuk92@gmail.com>
2019-05-23 00:35:18 +0900

    출퇴근 로그 추가 개발

--- service/db.js
+++ service/db.js
@@ -1,11 +1,12 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+var crypto = require('crypto');
 
 const adapter = new FileSync('conf/db.json');
 const db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ jiraInfoChannels: [] })
+db.defaults({ jiraInfoChannels: [], worklogs: [] })
   .write();
 
 function getDBState(cb) {
@@ -41,6 +42,49 @@ function setJiraInfo(params, cb) {
       .write();
     cb(res);
   });
+}
+
+
+function getHash(str) {
+  let shasum = crypto.createHash('sha1');
+  shasum.update(params.user);
+  const hash = shasum.digest('hex');
+  return hash;
+}
+// 출퇴근 시간을 가져옵니다.
+function getWorklog(params, cb) {
+  let userHash = getHash(param.user_id);
+  const res = db.get('worklogs').find({user: userHash}).value();
+  cb(res);
+}
+
+// 출퇴근 시간을 추가합니다.
+function addWorklog(params, cb) {
+  let userHash = getHash(param.user_id);
+}
+
+// 등록되어있는 출퇴근 시간을 변경합니다.
+function updateWorklog(params, cb) {
+  let userHash = getHash(param.user_id);
+}
+
+// 당일 출근 시간을 입력합니다.
+function addStartWorklog(params, cb) {
+  let userHash = getHash(param.user_id);
+  const res = db.get('worklogs')
+    .push({user: userHash, startTime: params.startWork})
+    .write();
+  return cb(res);
+}
+
+// 당일 퇴근 시간을 입력합니다.
+function addEndWorklog(params, cb) {
+  let userHash = getHash(param.user_id);
+  const res = db.get('worklogs')
+    .find({user: userHash})
+    .assign({endTime: params.endWork})
+    .write();
+  return cb(res);
 }
 
 module.exports = {
